5f014e499fb14fd762b3672998b23be05bf1c43b
MarcioBelucci <mdecarvalhobelucci@byupathway.edu>
2026-09-10 15:06:43 -0300

    Fix list element selection and improve button click event logic

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,9 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('list');
+const list = document.querySelector('#list');
 
 button.addEventListener('click', function() {
-    if (input.value.trim() !== '') {
+    if (input.value.trim() !== '' && list.children.length < 10) {
         const li = document.createElement('li');
         li.textContent = input.value;
         const deleteButton = document.createElement('button');
@@ -12,11 +12,10 @@ button.addEventListener('click', function() {
             list.removeChild(li);
             input.focus();
         });
+        li.appendChild(deleteButton);
+        list.appendChild(li);
         input.value = '';
-        input.focus();
     }
+    else if (list.children.length >=  10) { alert('You already hit the limit of 10!')}
+    input.focus();
 });
-
-li.append(deleteButton);
-list.append(li);
-input.value = '';
