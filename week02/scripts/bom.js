45062260897e3092029853795e7db850586a2cf2
Marcio Belucci <marcioacbelucci@gmail.com>
2026-09-10 14:30:18 -0300

    Refactor button click event to create and append list items dynamically

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,22 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('ul');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
+const list = document.querySelector('list');
+
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        li.textContent = input.value;
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+        input.value = '';
+        input.focus();
+    }
+});
+
 li.append(deleteButton);
 list.append(li);
+input.value = '';
