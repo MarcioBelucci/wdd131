1049e159d69699a14ad6e75914211924c48614c8
MarcioBelucci <mdecarvalhobelucci@byupathway.edu>
2026-09-19 11:56:03 -0300

    Enhance wind chill display and update CSS variables for improved styling consistency

--- scripts/place.js
+++ scripts/place.js
@@ -8,8 +8,9 @@ const calculateWindChill = (temp, windSpeed) =>
     13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16);
 let temp = 31;
 let windSpeed = 12;
+const windChill = document.querySelector("#windChill");
 if (temp <= 10 && windSpeed > 4.8) {
-    console.log(`${calculateWindChill(temp, windSpeed).toFixed(1)}°C`);
+    windChill.textContent = `${calculateWindChill(temp, windSpeed).toFixed(1)}°C`;
 } else {
-    console.log("N/A");
+    windChill.textContent = `N/A`;
 }
