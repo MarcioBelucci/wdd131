15a30eacb780f894a799ba5cb32b5939a6e5dd43
MarcioBelucci <mdecarvalhobelucci@byupathway.edu>
2026-09-18 23:58:52 -0300

    Add country page for Brazil with weather information and styles
    - Created place.html to showcase Brazil's culture and specifications.
    - Added weather-icon.svg for visual representation of weather conditions.
    - Implemented place.js for dynamic year and last modified date display.
    - Developed place.css and place-large.css for responsive styling and layout adjustments.

---
+++ scripts/place.js
@@ -0,0 +1,15 @@
+const currentYear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+const today = new Date();
+currentYear.textContent = today.getFullYear();
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+const calculateWindChill = (temp, windSpeed) =>
+    13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16);
+let temp = 20;
+let windSpeed = 5;
+if (temp <= 10 && windSpeed > 4.8) {
+    console.log(`${calculateWindChill(temp, windSpeed).toFixed(1)}°C`);
+} else {
+    console.log("N/A");
+}
